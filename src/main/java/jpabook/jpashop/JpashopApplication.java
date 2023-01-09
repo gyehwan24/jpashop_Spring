0d5f1b01a6b753de17f9b1b786d8a69be8732f44
Gyehwan Baek <b99hwan@gmail.com>
2023-01-09 20:14:09 +0900

    feat: api 기본

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -9,6 +9,10 @@ public class JpashopApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(JpashopApplication.class, args);
+
+		//버전확인
+//		String getVersion = org.springframework.core.SpringVersion.getVersion();
+//		System.out.println("버전---------:"+getVersion);
 	}
 
 }
